ea632ed0202a01d4072fc00d14abab9691884b74
Tom Jordan A. Esmale <152588148+notDanjo@users.noreply.github.com>
2023-12-19 12:28:15 +0800

    Remake the navbar of student side.

--- includes/nav2.php
+++ includes/nav2.php
@@ -1,30 +1,34 @@
-<?php 
+<?php
 require 'includes/db-inc.php';
+?>
 
-
- ?>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example">
-                <span class="sr-only">:</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="studentportal.php">Library Management System</a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="bs-example">
-            <ul class="nav navbar-nav">
-                <li ><a href="studentportal.php">Home</a></li>
-            <li><a href="profile.php">View Profile</a></li>
-                    <li><a href="borrow-student.php">Borrow Books</a></li>
-					<li><a href="fine-student.php">Fines</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-        </div>
+<body class="unique-class">
+    <div class="container">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-flex-container">
+                    <div class="navbar-section">
+                        <!-- Left navigation items go here -->
+                        <ul class="nav navbar-nav">
+                            <li class="active nav-item"><a href="studentportal.php">Home</a></li>
+                            <li class="nav-item"><a href="profile.php">View Profile</a></li>
+                            <li class="nav-item"><a href="borrow-student.php">Borrow Books</a></li>
+                            <li class="nav-item"><a href="fine-student.php">Fines</a></li>
+                        </ul>
+                    </div>
+                    <div class="navbar-header">
+                        <div class="logo">
+                            <img src="images/Logo.png">
+                        </div>
+                    </div>
+                    <div class="navbar-section">
+                        <!-- Right navigation items go here -->
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="logout.php">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </div>
-</nav>
+</body>
